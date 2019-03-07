50721162b393b552ce1292c786ea5fe795ddcf31
zmacmanx <zmacmanx@gmail.com>
2019-03-07 16:55:05 -0500

    Add hashing to the system

--- DevOpsSetup/scripts/common/JsonReader.py
+++ DevOpsSetup/scripts/common/JsonReader.py
@@ -2,20 +2,28 @@ import json
 import sys
 import os
 import subprocess
+import hashlib
 
 cmdPrefix = "az"
 objectList = open("objectList.txt", "r")
 for currentObject in objectList:
-  cmd = cmdPrefix + " " + currentObject + " list"       
+  cmdGroup = str(currentObject.strip())
+  cmd = cmdPrefix + " " + cmdGroup + " list"       
 
   process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=None, shell=True)
   output = process.communicate()
   data = json.loads(output[0])
 
+  print("Group: " + cmdGroup)
   for rec in data:
-    print(rec['id'])
-    #json.dump(rec, sys.stdout, indent=4)
+    dataRecord = str(rec)
+    dataRecordHash = hashlib.md5(dataRecord.encode())
+    print(str(dataRecordHash.hexdigest()) + " " + rec['id'])
     print
+    #json.dump(rec, sys.stdout, indent=4)
+  
+  print
+  print
 
 objectList.close()
 exit(0)
