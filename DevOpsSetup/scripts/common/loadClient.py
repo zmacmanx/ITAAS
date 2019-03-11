dada642d1817a441626f78cf43a49796aa3d2649
zmacmanx <zmacmanx@gmail.com>
2019-03-11 09:35:12 -0400

    adding company class

--- DevOpsSetup/scripts/common/loadClient.py
+++ DevOpsSetup/scripts/common/loadClient.py
@@ -9,10 +9,11 @@ class companyList:
   def listCompanies(self):
     for rec in self.data:
       json.dump(rec, sys.stdout, indent=4)
+    print()
 
 class companyObject:
-  def __init__(self, companyName):
-    self.companyName = companyName
+  def __init__(self, companyId):
+    self.companyId = companyId
     self.accountNumber = ""
 
   def loadCompanyInfo(self):
@@ -27,10 +28,12 @@ class companyObject:
     self.accountNumber = "xxxxxxxx"
 
   def printCompanyInfo(self):
-    print("Company: " + self.companyName)
+    print("Company: " + self.companyId)
     print("Account: " + self.accountNumber)
-
 
 if __name__ == '__main__':
   companyList = companyList()
   companyList.listCompanies()
+
+  # Find company
+  print("Id: " + companyList.data[1]["id"])
