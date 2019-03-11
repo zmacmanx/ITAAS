fd0b7563e7a09b91b959dab7dae2769f67e4e22f
zmacmanx <zmacmanx@gmail.com>
2019-03-11 14:56:27 -0400

    Add cloud list for each customer

--- DevOpsSetup/scripts/common/loadClient.py
+++ DevOpsSetup/scripts/common/loadClient.py
@@ -11,29 +11,51 @@ class companyList:
       json.dump(rec, sys.stdout, indent=4)
     print()
 
-class companyObject:
-  def __init__(self, companyId):
-    self.companyId = companyId
-    self.accountNumber = ""
+  def selectCompanyById(self, CompanyId):
+    #
+    # Find company
+    #
+    for cnt in range(0, len(self.data)):
+      # print("Id: " + self.data[cnt]["CompanyId"])
+      if self.data[cnt]["CompanyId"] == CompanyId:
+        return(self.data[cnt])
+      
+    return(None)
 
-  def loadCompanyInfo(self):
-    with open('companyList.json') as json_file:  
+  def selectCompanyByName(self, CompanyName):
+    #
+    # Find company
+    #
+    for cnt in range(0, len(self.data)):
+      # print("Id: " + self.data[cnt]["id"])
+      if self.data[cnt]["CompanyName"] == CompanyName:
+        return(self.data[cnt])
+      
+    return(None)  
+
+class company:
+  def __init__(self, CompanyId):
+    self.CompanyId = CompanyId
+    self.accountNumber = ""
+    self.clouds = []
+
+    with open('companyCloudList.json') as json_file:  
       data = json.load(json_file)
 
-      for rec in data:
-        print(rec['id'])
-        # json.dump(rec, sys.stdout, indent=4)
-        # print
-
-    self.accountNumber = "xxxxxxxx"
-
+      for rec in data: 
+        if rec["CompanyId"] == self.CompanyId:
+          self.clouds.append(rec) 
+          
   def printCompanyInfo(self):
-    print("Company: " + self.companyId)
+    print("Company: " + self.CompanyId)
     print("Account: " + self.accountNumber)
+    for cloud in self.clouds:
+      print(cloud)
 
 if __name__ == '__main__':
   companyList = companyList()
-  companyList.listCompanies()
+  # companyList.listCompanies()
+  # selectedCompany = companyList.selectCompany("3")
 
-  # Find company
-  print("Id: " + companyList.data[1]["id"])
+  currentCompany = company("2")
+  currentCompany.printCompanyInfo()
