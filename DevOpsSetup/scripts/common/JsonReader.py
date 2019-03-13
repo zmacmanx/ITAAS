82f2d3a354fd466e1ffc858506b2af11b8caab46
zmacmanx <zmacmanx@gmail.com>
2019-03-13 11:31:28 -0400

    change to json

--- DevOpsSetup/scripts/common/JsonReader.py
+++ DevOpsSetup/scripts/common/JsonReader.py
@@ -7,40 +7,13 @@ import hashlib
 class cloudObjects:
   def __init__(self):
     self.cloudObjectsList = []
-    self.cloudCategories = []
-    self.cloudElements = []
+    
+    with open('cloudElements.json') as json_file:  
+      data = json.load(json_file)
+      json_file.close()
 
-    data = open("objectList.txt", "r")
-    for currentObject in data:
-      self.cloudCategories.append(str(currentObject.strip()))
-    data.close()
-
-    data = open("elementList.txt", "r")
-    for currentObject in data:
-      self.cloudElements.append(str(currentObject.strip()))
-    data.close()
-
-    for clist in self.cloudCategories:
-      tmp1 = clist.split(":")
-      cloud = tmp1[0]
-      cmd = tmp1[1]
-      scmd = tmp1[2]
-      category = tmp1[3]
-
-      for elist in self.cloudElements:
-        tmp2 = elist.split(":")
-        element = ""
-
-        if tmp1[0] == tmp2[0]:
-          if tmp2[1] == "*":
-            element = tmp2[2]
-          else:
-            if tmp2[1] == tmp1[3]:
-              element = tmp2[2]
-        
-        if element != "":
-          idx = cloud + ":" + cmd + ":" + scmd + ":" + category + ":" + element
-          self.cloudObjectsList.append(idx)
+      for rec in data: 
+        self.cloudObjectsList.append(rec)
 
   def printCloudObjects(self):
     for data in self.cloudObjectsList:
@@ -51,43 +24,38 @@ class cloudObjects:
 
 class readCloudObjects:
   def __init__(self, cloudList):
-    self.dataMap = []
-    tarr = []
-    cmd = ""
+    self.dataMap = cloudList
+    self.cloudElements = []
 
-    for clist in cloudList:
-      tmp = clist.split(":")
-      cmdTmp = tmp[1] + " " + tmp[3] + " " + tmp[2]
+    for data in self.dataMap:
+      for cnt in range(0, len(data['CloudObjects'])):
+        cmd = data['Command'] + " " + data['CloudObjects'][cnt]['CloudEntity'] + " " + data['CloudObjects'][cnt]['Subcommand']
+        print(cmd)
 
-      if(cmd == ""):
-        cmd = cmdTmp
-
-      if cmd == cmdTmp:
-        tarr.append(tmp[4])
-      else:
-        print("Working On " + tmp[0] + tmp[3])
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=None, shell=True)
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=None, shell=True)    
         output = process.communicate()
-        data = json.loads(output[0])
+        cloudData = json.loads(output[0])
 
-        for rec in data:
+        for rec in cloudData:
           dataRecord = str(rec)
           dataRecordHash = hashlib.md5(dataRecord.encode())
           print(str(dataRecordHash.hexdigest()))
-          for ele in tarr:
-            print("       " + ele + ": " + rec[ele])
+          for xx in range(0, len(data['CloudObjects'][cnt]['Attributes'])):
+            if(data['Command'] == 'az'):
+              attr = data['CloudObjects'][cnt]['Attributes'][xx]
+              print("    " + attr + " " + rec[attr])
+            else:
+              print(str(rec))
 
-        # json.dump(rec, sys.stdout, indent=4)
-      
-        cmd = cmdTmp
-        for cnt in range(len(tarr), 0):
-          tarr[(cnt-1)].pop()
-        tarr.append(tmp[4])
-        print()
-        print()
+  def printDataMap(self):
+    for data in self.dataMap:
+      print(data)  
     
 if __name__ == '__main__':
   refList = cloudObjects()
-  readCloudObjects(refList.printResult())
+  # refList.printCloudObjects()
 
-  print(refList.printResult())
+  cloudMap = readCloudObjects(refList.printResult())
+  # cloudMap.printDataMap()
+
+  # print(refList.printResult())
